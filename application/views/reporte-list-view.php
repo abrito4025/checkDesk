11f1c4bef545fb28a7211cccfd5e54a1dd38b692
amosquea <amosquea@itla.edu.do>
2020-09-14 23:38:44 -0400

    Tercer commit

---
+++ application/views/reporte-list-view.php
@@ -0,0 +1,90 @@
+<!-- Page header -->
+<div class="full-box page-header">
+    <h3 class="text-left">
+        <i class="fas fa-clipboard-list fa-fw"></i> &nbsp; LISTA DE REPORTES
+    </h3>
+    <p class="text-justify">
+        <!--Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum delectus eos enim numquam fugit optio accusantium, aperiam eius facere architecto facilis quibusdam asperiores veniam omnis saepe est et, quod obcaecati.-->
+    </p>
+</div>
+
+<div class="container-fluid">
+    <ul class="full-box list-unstyled page-nav-tabs">
+       
+        <li>
+            <a class="active" href="mantenimiento-list/"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; LISTA DE REPORTES</a>
+        </li>
+      
+    </ul>
+</div>
+
+<!--CONTENT-->
+<div class="container-fluid">
+    <div class="table-responsive">
+        <table class="table table-dark table-sm">
+            <thead>
+                <tr class="text-center roboto-medium">
+                    <th>ID</th>
+                    <th>FECHA</th> 
+                    <th>MANTENIMIENTO</th>
+                    <th>DETALLE</th>
+                    <th>ESTADO</th>
+                    <th>USUARIO</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="text-center" >
+                    <td>1</td>
+                    <td>20/09/2020</td>
+                    <td>Reparación disco</td>
+                    <th>Suena mucho</th>
+                    <td>Pendiente</td>
+                    <td>3</td>
+                   
+                   
+                </tr>
+                <tr class="text-center" >
+                    <td>1</td>
+                    <td>20/09/2020</td>
+                    <td>Reparación disco</td>
+                    <th>Suena mucho</th>
+                    <td>Pendiente</td>
+                    <td>3</td>
+                   
+                   
+                </tr> <tr class="text-center" >
+                    <td>1</td>
+                    <td>20/09/2020</td>
+                    <td>Reparación disco</td>
+                    <th>Suena mucho</th>
+                    <td>Pendiente</td>
+                    <td>3</td>
+                   
+                    
+                </tr> <tr class="text-center" >
+                    <td>1</td>
+                    <td>20/09/2020</td>
+                    <td>Reparación disco</td>
+                    <th>Suena mucho</th>
+                    <td>Pendiente</td>
+                    <td>3</td>
+                   
+                   
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+            <li class="page-item disabled">
+                <a class="page-link" href="#" tabindex="-1">Previous</a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+                <a class="page-link" href="#">Next</a>
+            </li>
+        </ul>
+    </nav>
+</div>
